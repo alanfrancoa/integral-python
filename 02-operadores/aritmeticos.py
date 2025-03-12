604121fbed496095615c67ed5cd38f2d70a80ca6
Alan Alvarez <alvarezalanfranco@gmail.com>
2025-03-12 18:16:00 -0300

    Se crea proyecto de operadores aritmeticos.

---
+++ 02-operadores/aritmeticos.py
@@ -0,0 +1,56 @@
+# ------------------ TALLER INTEGRADOR DE PYTHON (UNLZ) ------------------
+# CLASE: 
+# PROFESOR: GABRIEL NIGLIO
+# CUATRIMESTRE: 1ER CUATRIMESTRE
+# AÑO: 2025
+# 
+
+# ------------------------- [OPERADORES] -------------------------
+
+'''
+En Python tenemos distintos tipos de operadores, que son los siguientes:
+- Aritméticos
+- Comparación
+- Lógicos
+- Asignación
+
+En esta sección vamos a ver los OPERADORES ARITMÉTICOS.
+'''
+
+# ------------------------- [OPERADORES ARITMÉTICOS] -------------------------
+
+'''
+Los operadores aritméticos son los operadores básicos que realizan operaciones matemáticas. Estos son:
+- Suma (+)
+- Resta (-)
+- Multiplicación (*)
+- División (/)
+- Módulo (%): Representa el resto de una división.
+'''
+
+# Definimos dos números para realizar las operaciones
+numero_1 = 77
+numero_2 = 44
+
+# Suma
+suma = numero_1 + numero_2
+
+# Resta
+resta = numero_1 - numero_2
+
+# Multiplicación
+multiplicacion = numero_1 * numero_2
+
+# División
+division = numero_1 / numero_2
+
+# Módulo (resto de la división)
+modulo = numero_1 % numero_2
+
+# Mostramos los resultados por pantalla
+print("------------ CALCULADORA ------------")
+print(f"La suma es: {suma}")
+print(f"La resta es: {resta}")
+print(f"La multiplicación es: {multiplicacion}")
+print(f"La división es: {division}")
+print(f"El resto es: {modulo}")
