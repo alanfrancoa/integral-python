625721571a1e35b2cead27642f66f7749831f584
Alan Alvarez <alvarezalanfranco@gmail.com>
2025-03-12 18:19:28 -0300

    Se realiza proyecto de comparadores

---
+++ 02-operadores/02-comparacion.py
@@ -0,0 +1,41 @@
+# ------------------ TALLER INTEGRADOR DE PYTHON (UNLZ) ------------------
+# CLASE: 
+# PROFESOR: GABRIEL NIGLIO
+# CUATRIMESTRE: 1ER CUATRIMESTRE
+# AÑO: 2025
+# 
+
+# ------------------------- [OPERADORES DE COMPARACIÓN] -------------------------
+
+'''
+Los operadores de comparación permiten comparar valores y devolver un valor booleano (True o False). Los operadores de comparación son los siguientes:
+
+- Igual (==)
+- Diferente (!=)
+- Mayor que (>)
+- Menor que (<)
+- Mayor o igual que (>=)
+- Menor o igual que (<=)
+'''
+
+# Definimos dos variables para realizar las comparaciones
+x = 10
+y = 3
+
+# Igual: Comparamos dos valores para determinar si son iguales
+print("Igual:", x == y)  # False
+
+# Diferente: Comparamos dos valores para determinar si son diferentes
+print("Diferente:", x != y)  # True
+
+# Mayor que: Comparamos si el primer valor es MAYOR al segundo valor
+print("Mayor que:", x > y)  # True
+
+# Menor que: Comparamos si el primer valor es MENOR al segundo valor
+print("Menor que:", x < y)  # False
+
+# Mayor o igual que: Comparamos si el primer valor es MAYOR O IGUAL al segundo valor
+print("Mayor o igual que:", x >= y)  # True
+
+# Menor o igual que: Comparamos si el primer valor es MENOR O IGUAL al segundo valor
+print("Menor o igual que:", x <= y)  # False
